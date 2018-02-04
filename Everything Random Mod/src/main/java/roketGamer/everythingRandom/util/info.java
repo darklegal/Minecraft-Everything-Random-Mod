af9876bf4e539bbd9d9cae90b3363d70d003cc76
roket gamer <roketminecrafter@gmail.com>
2018-02-03 22:04:19 -0700

    FINALLY fixed the item model issue
    about damn time

--- Everything Random Mod/src/main/java/roketGamer/everythingRandom/util/info.java
+++ Everything Random Mod/src/main/java/roketGamer/everythingRandom/util/info.java
@@ -2,7 +2,7 @@ package roketGamer.everythingRandom.util;
 
 public class info {
 	
-	public static final String MODID = "everythingRandom";
+	public static final String MODID = "everythingrandom";
 	public static final String NAME = "Everything Random!";
 	public static final String VERSION = "0.1";
 	public static final String CLIENT = "roketGamer.everythingRandom.proxy.clientProxy";
