84387bdd8540d9b8c3b6f9bdac140cc1992928f6
roket gamer <roketminecrafter@gmail.com>
2018-02-03 23:38:40 -0700

    Added many things
    Darkmetal,daymetal,nightmetal,randominium raw bit, smooth bit, rough nugget, smooth nugget, rough ingot, smooth ingot

--- Everything Random Mod/src/main/java/roketGamer/everythingRandom/init/itemInit.java
+++ Everything Random Mod/src/main/java/roketGamer/everythingRandom/init/itemInit.java
@@ -11,5 +11,14 @@ public class itemInit {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	public static final Item LIGHTMETAL_INGOT = new itemBase("ingot_lightmetal");
+	public static final Item DARKMETAL_INGOT = new itemBase("ingot_darkmetal");
+	public static final Item DAYMETAL_INGOT = new itemBase("ingot_daymetal");
+	public static final Item NIGHTMETAL_INGOT = new itemBase("ingot_nightmetal");
+	public static final Item RANDOMITE_BIT_RAW = new itemBase("bit_randominium_raw");
+	public static final Item RANDOMITE_BIT_SMOOTH = new itemBase("bit_randominium_smooth");
+	public static final Item RANDOMITE_NUGGET_RAW = new itemBase("nugget_randominium_raw");
+	public static final Item RANDOMITE_NUGGET_SMOOTH = new itemBase("nugget_randominium_smooth");
+	public static final Item RANDOMITE_INGOT_RAW = new itemBase("ingot_randominium_raw");
+	public static final Item RANDOMITE_INGOT_SMOOTH = new itemBase("ingot_randominium_smooth");
 
 }
